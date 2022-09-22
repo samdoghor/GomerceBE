bc86ccc3722ceb5af17b1b238c14acd807a72ae1
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2022-09-22 16:51:14 +0800

    payment method

--- src/models/paymentMethod.py
+++ src/models/paymentMethod.py
@@ -3,6 +3,7 @@ Define the paymentMethod model
 """
 from locale import currency
 from . import db
+from datetime import datetime
 from sqlalchemy_utils import CurrencyType, Currency
 
 
@@ -12,6 +13,8 @@ class payment_Method(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     currency = db.Column(CurrencyType)
+    created_at = db.Column(db.Datetime(), default = datetime.utcnow)
+    updated_at = db.Column(db.Datetime(), default = datetime.utcnow)
 
 
 def __init__(self, name='',currency=''):
