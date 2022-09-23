44883dc476f6a557fe7edab5d727a7ea30248593
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2022-09-24 05:26:47 +0800

    paymentmethod migrations

--- src/models/paymentMethod.py
+++ src/models/paymentMethod.py
@@ -7,14 +7,14 @@ from datetime import datetime
 from sqlalchemy_utils import CurrencyType, Currency
 
 
-class payment_Method(db.Model):
-    __tablename__ = 'paymentMethod'
+class paymentMethod(db.Model):
+    __tablename__ = 'payment_methods'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     currency = db.Column(CurrencyType)
-    created_at = db.Column(db.Datetime(), default = datetime.utcnow)
-    updated_at = db.Column(db.Datetime(), default = datetime.utcnow)
+    created_at = db.Column(db.DateTime(), default = datetime.utcnow)
+    updated_at = db.Column(db.DateTime(), default = datetime.utcnow)
 
 
 def __init__(self, name='',currency=''):
@@ -22,10 +22,11 @@ def __init__(self, name='',currency=''):
     self.currency = currency
 
 
-payment = payment_Method()
+payment = paymentMethod()
 payment.currency = Currency('NGN')
-db.session.add(payment)
-db.session.commit()
+
+# db.session.add(payment)
+# db.session.commit()
 
 payment.currency # Currency('NGN')
 payment.currency.name # Nigerian Naira
