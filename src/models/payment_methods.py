7a228f006e61663e7d2db9b1c5640c44e2572ff8
aaronben41 Github <aaronben41@gmail.com>
2022-09-26 13:16:42 +0100

    Updated payment methods model

---
+++ src/models/payment_methods.py
@@ -0,0 +1,22 @@
+"""
+Define the Payment Methods model
+"""
+from . import db
+from .abc import BaseModel, MetaBaseModel
+from datetime import datetime
+
+
+
+class PaymentMethods(db.Model, BaseModel, metaclass=MetaBaseModel):
+    """ The Payment Methods model """
+
+    __tablename__ = "payment_methods"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    currency = db.Column(db.String(32), nullable=False)
+    created_at = db.Column(db.DateTime(), default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime(), default=datetime.utcnow)
+
+    #Relationship
+    payment_details = db.relationship('paymentDetails', backref='payment_methods', lazy=True)
