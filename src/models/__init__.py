4698a025fba52a55e54afdb60d15008339e3c5b1
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2022-10-07 01:51:48 +0800

    payMentmethod

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,9 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 from .customer import Customer
-<<<<<<< HEAD
 from .paymentMethod import paymentMethod
-=======
 from .verification_token import VerificationToken
 from .product import Product
->>>>>>> b0876a7bff72364ceaf8fdb37c1cbb4397879318
+
